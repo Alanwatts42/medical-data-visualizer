106eb4322155d20e7b404c9058786d573c5cfe1f
Alanwatts42 <evanzemail@gmail.com>
2024-06-07 20:24:14 -0500

    fixed some errors in medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -2,32 +2,23 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np    
-## %%
 # 1 - Import the data from medical_examination.csv and assign it to the df variable
 
 df = pd.read_csv('medical_examination.csv')
 
-# %%
 # 2 - Create the overweight column in the df variable
 df['overweight'] =(df['weight'] / (df['height'] / 100) ** 2).apply(lambda x: 1 if x > 25 else 0))
 
-# %% [markdown]
 # 3 - Normalize data by making 0 always good and 1 always bad. If the value of cholesterol or gluc is 1, set the value to 0. If the value is more than 1, set the value to 1
-%
 df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
 df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1
 )
 
-# %%
 # 4 - Draw the Categorical Plot in the draw_cat_plot function
 def draw_cat_plot(): 
     # 5 - Create a DataFrame for the cat plot using pd.melt with values from cholesterol, gluc, smoke, alco, active, and overweight in the df_cat variable.
-    df_cat = pd.melt(data
-                    , id_var  ['cardio']
-                    , value_var=[
-           s =           ['cholesterol', 'gluc', 'smoke', 'alco', 'active'
-                        , 'overweight
-                     '])
+    df_cat = pd.melt(id_vars = ['cardio'] , value_vars =  ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], frame = df)
+
 
     # 6 - Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
     df_cat = df_cat.groupby(['cardio', 'variable', 'value'], as_inde  False)
@@ -40,7 +31,6 @@ def draw_cat_plot():
                , co  'cardio'
                , kin  'bar'
 
-
     # 8 - Get the figure for the output
     fig = None
 
@@ -49,7 +39,6 @@ def draw_cat_plot():
     return fig
 
 
-# %%
 # 10 - Draw the Heat Map in the draw_heat_map function
 def draw_heat_map(): 
     # 11 - Clean the data
@@ -70,8 +59,3 @@ def draw_heat_map():
     # 16 - Do not modify the next two lines
     fig.savefig('heatmap.png')
     return fi
-g
-
-# %%
-
-# %%
