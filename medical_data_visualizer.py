b4a53b4925b4a6af031a9e2846cbbc09d0eb4b56
Evan Sherwood <evanzemail@gmail.com>
2024-06-22 19:15:02 +0000

    gitpod.io edits to main python file named after project

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -32,7 +32,7 @@ def draw_heat_map():
     # diastolic pressure is higher than systolic (Keep the correct data with (df['ap_lo'] <= df['ap_hi']))
     df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025))]
     # height is less than the 2.5th percentile (Keep the correct data with (df['height'] >= df['heigth'].quantile(0.025)))
-    df_heat = df_heat[df_heat['height'] <= df_heat['height'].quantile(0.975)]
+    df_heat = df_heat[df_heat['height'] >= df_heat['height'].quantile(0.975)]
     # weight is less than the 2.5th percentile
     df_heat = df_heat[(df_heat['weight'] >= df_heat['weight'].quantile(0.025))]
     # weight is more than the 97.5th percentile
