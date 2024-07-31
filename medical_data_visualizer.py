fcee7ba0a01b6cb80cfec8cea2a204a134e4e46f
Alanwatts42 <evanzemail@gmail.com>
2024-07-30 23:45:07 -0500

    slowly making progress

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -40,7 +40,7 @@ def draw_heat_map():
     # weight is more than the 97.5th percentile
     df_heat = df['weight'] <= df['weight'].quantile(0.975)
     # 12 - Calculate the correlation matrix
-    corr = df_heat.corr(other=None)
+    corr = df_heat.corr(df['overweight'], method='pearson', min_periods=1)
     # 13 - Generate a mask for the upper triangle
     mask = np.triu(corr)
     # 14 - Set up the matplotlib figure
